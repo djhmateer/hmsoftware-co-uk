e410a39dd329f43d7c45294b0b3268a872f3b55b
Dave Mateer <davemateer@gmail.com>
2021-02-23 09:58:48 +0000

    auto

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,16 @@
 module.exports = {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    }
+    // plugins: {
+    //   tailwindcss: {},
+    //   autoprefixer: {},
+    // }
+    plugins: [
+      require('tailwindcss'),
+      require('autoprefixer'),
+      // export NODE_ENV=production
+      // export NODE_ENV=development
+      // minification
+      // require('cssnano')({
+      //     preset: 'default',
+      //   }),
+  ]
   }
